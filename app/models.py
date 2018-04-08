8f32f7e90d0ac9d830ed8d2d15d87bd9fe5005e8
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-04-08 23:24:41 +0200

    Models Updated

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class OfficeAddress(models.Model):
+    address = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+class CitizenAddress(models.Model):
+    id_number = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
