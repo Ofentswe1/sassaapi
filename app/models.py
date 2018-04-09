58d964f66f2fc433db319e7882fdbb87c7fb74fe
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-04-09 09:08:21 +0200

    added payment model for balances and next payment

--- app/models.py
+++ app/models.py
@@ -12,3 +12,10 @@ class CitizenAddress(models.Model):
     address = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+class Payments(models.Model):
+    username = models.CharField(max_length=13)
+    balance = models.FloatField()
+    pay_date = models.DateField()
+    collected = models.CharField(max_length=3)
+    collected_date = models.DateTimeField()
