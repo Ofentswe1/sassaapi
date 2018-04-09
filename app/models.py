9783f54faf674451e6243592b6e3d242e24c34aa
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-04-09 18:22:03 +0200

    Address Type Added

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class OfficeAddress(models.Model):
+    office_type = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
